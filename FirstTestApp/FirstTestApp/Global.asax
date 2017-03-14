b01ac001d44f74299109ee4e71951945ad51e739
bazith-activehealth <BBahurudeen@activehealth.net>
2017-03-14 12:40:53 +0530

    FirstApp
    Created first app for test

---
+++ FirstTestApp/FirstTestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstTestApp.MvcApplication" Language="C#" %>
---
+++ FirstTestApp/FirstTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstTestApp.Startup))]
+namespace FirstTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
